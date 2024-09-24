d1d03edf79e03765ba768da894e95195c59736d9
skrsumit250 <skrsumit250@gmail.com>
2024-09-24 16:54:55 +0530

    build pages

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,18 @@ function App(){
   return (
     <>
     <div className="home">
-      <h1>Welcome to BSBE Alumni Page.</h1>
-      <h2>The page is under development.</h2>
+      <img src="/bg.jpg" alt="" />
+      <div className="box">
+        <h1>Welcome to BSBE Alumni Page.</h1>
+        <h3>BSBE Alumni Meet 2024, October 26 - 27, 2024, Oct 26, 2024 to Oct 27, 2024</h3>
+        <h3>Dept. of Biosciences and Bioengineering will be hosting their first alumni meet on 26th and 27th October, 2024. Regular updates will be posted on</h3>
+        <a href="https://www.linkedin.com/in/dept-of-biosciences-and-bioengineering-iit-guwahati-11980a325/">LinkedIn</a>
+        <a href="https://www.instagram.com/bsbe_iitguwahati/">Instagram</a>
+        <a href="https://x.com/BSBE_IITG">Twitter</a>
+        <h4>All the alumi are requeted to register: <a href="https://docs.google.com/forms/d/e/1FAIpQLSf1U22vyZFMzBCH9R7GeCQbGPR0Y9CsccOKWDkRsElcawCCrQ/viewform">Form</a></h4>
+      </div>
     </div>
+    
     
     {/* {data.length > 0 ? (
       data.map((item, idx) => (
