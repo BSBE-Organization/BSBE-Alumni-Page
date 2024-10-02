f5eaeb455eacd7cd85ae07131660b4e3df56de9b
Adi21032004 <nonglenadi@gmail.com>
2024-10-03 00:57:43 +0530

    added the login and user form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Login from './assets/Login';
+import AuthPage from './components/Login/page';
+import './index.css';
+import UserProfileForm from './components/UserForm/Page';
 function App(){
   
   return (
     <>
-      <Login/>
+      {/* <Login/> */}
+      {/* <AuthPage/> */}
+      <UserProfileForm/>
     </>
   )
 }
 export default App;
+
