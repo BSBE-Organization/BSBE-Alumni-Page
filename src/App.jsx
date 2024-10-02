fdd09e2747c4e12a5c0dbbf6653ebd055eb83f94
skrsumit250 <skrsumit250@gmail.com>
2024-10-02 22:43:55 +0530

    add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Login from './assets/Login';
+import LandingPage from './components/LandingPage';
 function App(){
   
   return (
     <>
-      <Login/>
+      <LandingPage/>
     </>
   )
 }
