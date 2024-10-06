92cd25c0b524f7fca965b2551837176704b7c7ca
skrsumit250 <skrsumit250@gmail.com>
2024-10-06 18:41:36 +0530

    change styles of cover img

--- src/components/LandingPage/landingPage.jsx
+++ src/components/LandingPage/landingPage.jsx
@@ -4,12 +4,11 @@ import Events from "../LandingPage/Events";
 const LandingPage = () => {
   return (
     <>
-      <div className="landing-container">
-        <div className="content">
-          <h1 style={{ color: "#0f6f41" }}>BSBE</h1> {/* Changed here */}
-          <h1 className="text-6xl md:text-6xl lg:text-8xl text-[#000000]">
-            Alumni Page
-          </h1>
+      <div className="home">
+        <img src="home/bg.jpg" alt="" />
+        <div className="header">
+          <h2>BSBE</h2>
+          <h1>Alumni Page</h1>
         </div>
       </div>
       <Stats />
