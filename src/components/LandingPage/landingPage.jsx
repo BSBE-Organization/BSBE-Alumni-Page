ae79b7456c652fded6f7ed78fda757bed07195ff
bhati-udai <udaibhati07@gmail.com>
2024-10-06 18:02:06 +0530

    Background Update

--- src/components/LandingPage/landingPage.jsx
+++ src/components/LandingPage/landingPage.jsx
@@ -1,12 +1,38 @@
-import "./styles/landingPage.css";
 import Stats from "../LandingPage/stats";
 import Events from "../LandingPage/Events";
+
 const LandingPage = () => {
   return (
     <>
-      <div className="landing-container">
-        <div className="content">
-          <h1 style={{ color: "#0f6f41" }}>BSBE</h1> {/* Changed here */}
+      <div
+        className="landing-container"
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "80vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden",
+          margin: "0",
+        }}
+      >
+        {/* Background Image */}
+        <img
+          src="/home/bg.jpg"
+          alt="Background"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            zIndex: 1,
+          }}
+        />
+        <div className="content" style={{ position: "relative", zIndex: 2 }}>
+          <h1 style={{ color: "#0f6f41" }}>BSBE</h1>
           <h1 className="text-6xl md:text-6xl lg:text-8xl text-[#000000]">
             Alumni Page
           </h1>
@@ -17,4 +43,5 @@ const LandingPage = () => {
     </>
   );
 };
+
 export default LandingPage;
